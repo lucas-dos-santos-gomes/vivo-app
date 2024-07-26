7187700d4a45b69ca06d6b2cd3008fdc04f6c721
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-07-26 18:12:24 -0300

    First Column

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Container(
-        width: 150,
-        height: 150,
-        color: Colors.purple,
-      ),
+    Column(
+      children: [
+        Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            color: Colors.purple,
+          ),
+        ),
+        Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            color: Colors.deepPurple,
+          ),
+        ),
+        Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            color: Colors.purpleAccent,
+          ),
+        ),
+      ],
     ),
   );
 }
