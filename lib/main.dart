d3c18e360115940fea841fc1de68badd334b142f
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-07-26 18:09:56 -0300

    First Container

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     Center(
-      child: Text(
-        'Hello, World!', 
-        textDirection: TextDirection.ltr,
+      child: Container(
+        width: 150,
+        height: 150,
+        color: Colors.purple,
       ),
     ),
   );
