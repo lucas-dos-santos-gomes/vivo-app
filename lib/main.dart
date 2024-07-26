cdb0e6343945866817dd616544a35ddb26a248de
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-07-26 18:40:58 -0300

    Challenge of first class

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: [
-        Center(
-          child: Container(
-            width: 150,
-            height: 150,
-            color: Colors.purple,
+    Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              width: 150, 
+              height: 150, 
+              color: Colors.deepPurple,
+            ),
           ),
-        ),
-        Center(
-          child: Container(
-            width: 150,
-            height: 150,
-            color: Colors.deepPurple,
+          Center(
+            child: Container(
+              width: 120, 
+              height: 120, 
+              color: Colors.deepPurpleAccent,
+            ),
           ),
-        ),
-        Center(
-          child: Container(
-            width: 150,
-            height: 150,
-            color: Colors.purpleAccent,
+          Center(
+            child: Container(
+              width: 90, 
+              height: 90, 
+              color: Colors.purple,
+              child: Center(
+                child: Text(
+                  'Vivo', 
+                  textDirection: TextDirection.ltr,
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
